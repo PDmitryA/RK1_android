3a1f218f5c9906ae8f6ac020b7a52fd3d5d3aace
Dmitry Poponkin <mitia.poponkin@yandex.ru>
2018-12-24 03:59:23 +0300

    2 ViewHolders WOWOWOOWOWOWOWOWOWOWOWOW

--- app/src/main/java/com/example/pda/rk1_android/NumberAdapter.java
+++ app/src/main/java/com/example/pda/rk1_android/NumberAdapter.java
@@ -10,12 +10,17 @@ import android.widget.Button;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.ListIterator;
 
-public class NumberAdapter extends RecyclerView.Adapter<NumberAdapter.NumberViewHolder> {
+public class NumberAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private List<NumberedButton> numberedButtonsList = new ArrayList<>();
 
-    class NumberViewHolder extends RecyclerView.ViewHolder {
+    interface ClickableNumberedButton {
+        void bind(NumberedButton nb);
+    }
+
+    class NumberViewHolder extends RecyclerView.ViewHolder implements ClickableNumberedButton {
         private Button numberedButton;
 
         NumberViewHolder(View itemView) {
@@ -23,8 +28,21 @@ public class NumberAdapter extends RecyclerView.Adapter<NumberAdapter.NumberView
             numberedButton = itemView.findViewById(R.id.numbered_button);
         }
 
-        void bind(NumberedButton nb) {
-            updateButton(numberedButton, nb);
+        public void bind(NumberedButton nb) {
+            initButton(numberedButton, nb);
+        }
+    }
+
+    class ClickedNumberViewHolder extends RecyclerView.ViewHolder implements ClickableNumberedButton {
+        private Button clickedNumberedButton;
+
+        ClickedNumberViewHolder(View itemView) {
+            super(itemView);
+            clickedNumberedButton = itemView.findViewById(R.id.clicked_numbered_button);
+        }
+
+        public void bind(NumberedButton nb) {
+            initButton(clickedNumberedButton, nb);
         }
     }
 
@@ -34,21 +52,23 @@ public class NumberAdapter extends RecyclerView.Adapter<NumberAdapter.NumberView
             Button clicked = (Button) v;
             Integer number = (Integer) clicked.getTag();
 
-            for (NumberedButton nb: numberedButtonsList) {
+            ListIterator iter = numberedButtonsList.listIterator();
+            while (iter.hasNext()) {
+                NumberedButton nb = (NumberedButton) iter.next();
                 if (nb.getNumber().equals(number)) {
                     nb.getOnClick().numberClickedHandler(nb.getClicked() ? -number : number);
                     nb.setClicked(!nb.getClicked());
-                    updateButton(clicked, nb);
+
+                    notifyItemChanged(iter.previousIndex());
+                    return;
                 }
             }
         }
     };
 
-    private void updateButton(Button button, NumberedButton numberedButton) {
+    private void initButton(Button button, NumberedButton numberedButton) {
         button.setText(String.valueOf(numberedButton.getNumber()));
-        int background = numberedButton.isClicked ? R.color.colorPrimary : R.color.colorGreen;
         button.setTag(numberedButton.getNumber());
-        button.setBackgroundResource(background);
         button.setOnClickListener(onClickListener);
     }
 
@@ -63,20 +83,35 @@ public class NumberAdapter extends RecyclerView.Adapter<NumberAdapter.NumberView
     }
 
     @Override
+    public int getItemViewType(int position) {
+        if (numberedButtonsList.get(position).getClicked()) {
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
     public int getItemCount() {
         return numberedButtonsList.size();
     }
 
     @NonNull
     @Override
-    public NumberViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.numbered_button, parent, false);
-        return new NumberViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case 0:
+                View view0 = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.numbered_button, parent, false);
+                return new NumberViewHolder(view0);
+            default:
+                View view1 = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.clicked_numbered_button, parent, false);
+                return new ClickedNumberViewHolder(view1);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull NumberViewHolder holder, int position) {
-        holder.bind(numberedButtonsList.get(position));
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ((ClickableNumberedButton)holder).bind(numberedButtonsList.get(position));
     }
 }
